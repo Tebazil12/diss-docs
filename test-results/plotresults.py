4918e2bf3deaa7ff4413dbd87748be31a6e8b164
Tebazil12 <lizzie.stone@hotmail.com>
2017-05-02 10:44:12 +0100

    Add more images of proccessed results

--- test-results/plotresults.py
+++ test-results/plotresults.py
@@ -1,19 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
 import csv
 import re
 
 #http://www.scipy-lectures.org/intro/matplotlib/auto_examples/plot_quiver_ex.html
-#https://matplotlib.org/basemap/users/examples.html
-#map = Basemap(projection='ortho',lat_0=54,lon_0=-3,resolution='l')
-#map.drawcoastlines(linewidth=0.25)
-#map.drawcountries(linewidth=0.25)
-#map.fillcontinents(color='coral',lake_color='aqua')
-# draw the edge of the map projection region (the projection limb)
-#map.drawmapboundary(fill_color='aqua')
 
-loc_file = open("big-feild-westward/logs-for-vis-vecs.csv", "rU") 
+loc_file = open("castle-simplescan1/logs-to-vec.csv", "rU") 
 reader = csv.reader(loc_file)
 list_1=[]
 x = []
@@ -39,48 +31,40 @@ for i,row in enumerate(reader):
             x.append(float(row[0]))
             y.append(float(row[1]))
             b.append(int(m))
-        
-        
-    #list_1.append(Location(float(row[0]),float(row[1])))
+
 loc_file.close()
 
-
-
-#print x,y,b
 
 y_b = np.cos(np.radians(b))
 x_b = np.sin(np.radians(b))
 
-#print y_b
-#print x_b
-#print "locs:"
-#for i in perimeter:
-#    print i    
 
+plt.quiver(y, x, x_b, y_b,width=0.001,scale=1 / 0.015) #http://matplotlib.org/examples/pylab_examples/quiver_demo.html
 
+loc_file = open('castle-simplescan1/waypoints.csv', "r") 
+reader = csv.reader(loc_file)
+ways_t=[]
+ways_l=[]
+for row in reader:
+    ways_t.append(float(row[0]))
+    ways_l.append(float(row[1]))
+loc_file.close()
 
-#n = 8
-#X, Y = np.mgrid[0:n, 0:n]
-#T = np.arctan2(Y - n / 2., X - n/2.)
-#R = 10 + np.sqrt((Y - n / 2.0) ** 2 + (X - n / 2.0) ** 2)
-#U, V = R * np.cos(T), R * np.sin(T)
+plt.plot(ways_l,ways_t)
+plt.scatter(ways_l,ways_t)
 
-#plt.axes([0.025, 0.025, 0.95, 0.95])
-#map.quiver(y, x, x_b, y_b,linewidths=0.1)#, R)#, alpha=.5)
-plt.quiver(y, x, x_b, y_b,width=0.001,scale=1 / 0.015)#, R)#, alpha=.5)#http://matplotlib.org/examples/pylab_examples/quiver_demo.html
-#plt.quiver(X, Y, U, V, edgecolor='k', facecolor='None', linewidth=.5)
+#loc_file2 = open('castle-simplescan1/water.csv', "r") 
+#reader2 = csv.reader(loc_file2)
+#water_t=[]
+#water_l=[]
+#for row in reader2:
+#    water_t.append(float(row[0]))
+#    water_l.append(float(row[1]))
+#loc_file2.close()
 
-#w_x =52.4003569,52.4008446,52.4008315,52.4003405,52.3996090
-#w_y = -3.8704652,-3.8704813,-3.8697410,-3.8697892,-3.8690946
+#plt.plot(water_l,water_t)
+#plt.scatter(water_l,water_t)
+  
 
-
-
-
-
-#plt.xlim(-1, n)
-#plt.xticks(())
-#plt.ylim(-1, n)
-#plt.yticks(())
-#map.scatter(w_y,w_x)
-#plt.scatter(w_y,w_x)
+plt.plot(y,x,'r-')
 plt.show()
